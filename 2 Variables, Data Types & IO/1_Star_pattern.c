0dcdebdb170db80f8a032f6284d52d728acbc638
ranjeetRai25 <ranjeetkumarhit97@gmail.com>
2025-11-12 19:26:15 +0530

    Star patter

---
+++ 2 Variables, Data Types & IO/1_Star_pattern.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+
+int main()
+{
+    printf("Half pyramid");
+
+    printf("*\n");
+    printf("* *\n");
+    printf("* * *\n");
+    printf("* * * *\n");
+    printf("* * * * *\n");
+
+    printf("\n");
+
+    printf("Reverse Right Half pyramid");
+
+    printf("* * * * *\n");
+    printf("* * * * \n");
+    printf("* * * \n");
+    printf("* * \n");
+    printf("* \n");
+
+    printf("Left Half pyramid");
+
+    printf("       *\n");
+    printf("     * *\n");
+    printf("   * * *\n");
+    printf(" * * * *\n");
+    
+    return 0;
+
+
+}
+
---
+++ 2 Variables, Data Types & IO/2_Star_pattern.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+// Star pattern with one print satement
+
+int main()
+{
+    printf("Half pyramid\n");
+
+    printf(" *\n * *\n * * *\n * * * *\n");
+    
+
+    printf("Reverse Right Half pyramid\n");
+    printf(" * * * *\n * * *\n * *\n *\n");
+
+
+    printf("Left Half pyramid\n");
+    printf("        *\n      * *\n    * * *\n  * * * *\n");
+
+
+
+    
+    
+    return 0;
+
+
+}
